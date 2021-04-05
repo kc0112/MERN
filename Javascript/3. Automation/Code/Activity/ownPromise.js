7918faaaa8d163cbba2c14d05dd8d64bee83d89a
kc0112 <Khushichavada0112@gmail.com>
2021-04-05 11:42:48 +0530

    ownPromise

--- Javascript/3. Automation/Code/Activity/ownPromise.js
+++ Javascript/3. Automation/Code/Activity/ownPromise.js
@@ -1,17 +1,5 @@
 const fs = require("fs");
 
-// function name (para){
-// 	return new Promise(function (resolve, reject) {
-// 		yha likho function jisko promisify krna
-// 		with callback 
-// 		function cb(err, data) {
-// 			if (err) {
-// 				reject(err);
-// 			} else {
-// 				resolve(data);
-// 			}
-// 	})
-// }
 function promisifiedFileRead(filePath) {
 	return new Promise(function (resolve, reject) {
 		fs.readFile(filePath, function cb(err, data) {

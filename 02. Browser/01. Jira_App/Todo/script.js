61a2f244682052b71b39942be757ef0254791b2a
kc0112 <Khushichavada0112@gmail.com>
2021-05-04 16:33:31 +0530

    update

--- 02. Browser/01. Jira_App/Todo/script.js
+++ 02. Browser/01. Jira_App/Todo/script.js
@@ -1,16 +1,38 @@
+let arr = [];
 let input = document.querySelector(".input_box");
 let ul = document.querySelector(".tasklist");
-input.addEventListener("keydown", function (e) {
-    if (e.key == "Enter") {
-   
-        let task = input.value;
-        input.value = "";
+
+if (localStorage.getItem("allTask")) {
+    let stringArr = localStorage.getItem("allTask");
+    arr = JSON.parse(stringArr);
+
+    for (let i = 0; i < arr.length; i++){
         let li = document.createElement("li");
-        li.setAttribute("class", "task");
-        li.innerText = task;
+        
+        li.innerText = arr[i];
         li.addEventListener("dblclick", function () {
             li.remove();
         })
         ul.appendChild(li);
+        li.setAttribute("class", "task");
+    }
+}
+
+input.addEventListener("keydown", function (e) {
+    if (e.key == "Enter") {
+        let task = input.value;
+        
+        let li = document.createElement("li");
+        
+        li.innerText = task;
+        arr.push(task);
+        let stringArr = JSON.stringify(arr);
+        localStorage.setItem("allTask", stringArr);
+        li.addEventListener("dblclick", function (e) {
+            li.remove( );
+        })
+        ul.appendChild(li);
+        input.value = "";
+        li.setAttribute("class", "task");
     }
 });

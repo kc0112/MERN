bc47528b0d7fc949937b9779ec848fe69699e17d
kc0112 <Khushichavada0112@gmail.com>
2021-07-06 11:12:51 +0530

    hooks

---
+++ 03.React/hooks/src/App.js
@@ -0,0 +1,20 @@
+import Uswo from "./useState/Uswo";
+// import Ue1 from "./useEffect/Ue1";
+import Ue2 from "./useEffect/Ue2";
+import Ue3 from "./useEffect/Ue3";
+import Infinite from "./useEffect/Infinite";
+import Uec1 from "./useEffect/UseEffect_Clean_up/Uec1";
+function App() {
+  return (
+    <>
+    {/* <Uswo/> */}
+    {/* <Ue1/> */}
+{/* <Ue2/> */}
+    {/* <Infinite/> */}
+    {/* <Ue3/> */}
+    <Uec1/>
+    </>
+  );
+}
+
+export default App;

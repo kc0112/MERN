eaed9ccd10b0e3b615c6b98a6fc17b77514b8da5
Khushi Chavada <khushichavada0112@gmail.com>
2021-07-21 13:52:06 +0530

    update

--- 03.React/reels/src/components/Posts.js
+++ 03.React/reels/src/components/Posts.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import Card from '@material-ui/core/Card';
@@ -20,53 +20,53 @@ import Likes from './Likes';
 import AddComment from './AddComment';
 import Comments from './Comments';
 const useStyles = makeStyles({
-    root: {
-      width: '100%',
-      padding: '0px'
-    },
-    loader: {
-      position: 'absolute',
-      left: '50%',
-      top: '50%'
-    },
-    typo: {
-      marginLeft: '2%'
-    },
-    vac: {
-      marginLeft: '3.5%',
-      color: '#8e8e8e',
-      cursor:'pointer'
-    },
-    dp: {
-      marginLeft: '2%'
-    },
-    cc: {
-      height: '50vh',
-      overflowY: 'auto'
-    },
-    seeComments:{
-      height:'54vh',
-      overflowY:'auto'
-    },
-    ci:{
-    
-      color: 'white',
-      left:'9%',
-      cursor:'pointer'
-    },
-    mn:{
-      color:'white',
-      
-     
-    },
-    tmn:{
-      color:'white'
-    }
-  
-  });
-function Posts({userData=null}) {
+  root: {
+    width: '100%',
+    padding: '0px'
+  },
+  loader: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%'
+  },
+  typo: {
+    marginLeft: '2%'
+  },
+  vac: {
+    marginLeft: '3.5%',
+    color: '#8e8e8e',
+    cursor: 'pointer'
+  },
+  dp: {
+    marginLeft: '2%'
+  },
+  cc: {
+    height: '50vh',
+    overflowY: 'auto'
+  },
+  seeComments: {
+    height: '54vh',
+    overflowY: 'auto'
+  },
+  ci: {
+
+    color: 'white',
+    left: '9%',
+    cursor: 'pointer'
+  },
+  mn: {
+    color: 'white',
+
+
+  },
+  tmn: {
+    color: 'white'
+  }
+
+});
+function Posts({ userData = null }) {
   const classes = useStyles();
-  const[posts,setPosts] = useState(null);
+  const [posts, setPosts] = useState(null);
   const [openId, setOpenId] = useState(null);
   const handleClickOpen = (id) => {
     setOpenId(id);
@@ -74,103 +74,103 @@ function Posts({userData=null}) {
   const handleClose = () => {
     setOpenId(null);
   };
-      const callback = entries=>{
-        entries.forEach(element => {
-            console.log(element);
-            let el = element.target.childNodes[0];
-            el.play().then(()=>{
-                //if this video is not in viewport then pause it
-                if(!el.paused && !element.isIntersecting)
-                {
-                    el.pause();                
-                }
-            })
-
-        });
-    }
-    const observer = new IntersectionObserver(callback,{ threshold:0.85 });
-    useEffect(()=>{
-      let parr=[];
-      const unsub = database.posts.orderBy('createdAt','desc').onSnapshot(querySnapshot=>{
-        parr=[];
-        querySnapshot.forEach((doc)=>{
-          console.log(doc.data(),+"  "+doc.id);
-          let data = {...doc.data(),postId:doc.id}
-          parr.push(data)
-        })
-        setPosts(parr);
-
+  const callback = entries => {
+    entries.forEach(element => {
+      console.log(element);
+      let el = element.target.childNodes[0];
+      el.play().then(() => {
+        //if this video is not in viewport then pause it
+        if (!el.paused && !element.isIntersecting) {
+          el.pause();
+        }
       })
-      return unsub;
-    }
-    ,[])
-    useEffect(()=>{
-      let elements = document.querySelectorAll('.videos');
-      elements.forEach(el=>{
-        observer.observe(el);
+
+    });
+  }
+  const observer = new IntersectionObserver(callback, { threshold: 0.85 });
+  useEffect(() => {
+    let parr = [];
+    const unsub = database.posts.orderBy('createdAt', 'desc').onSnapshot(querySnapshot => {
+      parr = [];
+      querySnapshot.forEach((doc) => {
+        let data = { ...doc.data(), postId: doc.id }
+        parr.push(data)
       })
-      return ()=>{
-        observer.disconnect();
-      }
-    },[posts])
-    return (
-      <>
-        <div className='place'>
-        </div>
-        {posts==null?<CircularProgress className={classes.loader} color="secondary" />:
+      setPosts(parr);
+
+    })
+    // cleanup jb is page se chle jao
+    return unsub;
+  }
+    , [])
+  useEffect(() => {
+    let elements = document.querySelectorAll('.videos');
+    elements.forEach(el => {
+      observer.observe(el);
+    })
+    // naye vdos aenge unpr dobara listener nhi attack krna 
+    return () => {
+      observer.disconnect();
+    }
+  }, [posts])
+  return (
+    <>
+      <div className='place'>
+      </div>
+      {posts == null ? <CircularProgress className={classes.loader} color="secondary" /> :
         <div className='video-container' id='video-container'>
           {
-            posts.map((post)=>(
+            posts.map((post) => (
               <React.Fragment key={post.postId}>
                 <div className='videos'>
-                  <Video source={post.pUrl} id={post.pId}/>
-                  <div className='fa' style={{display:'flex'}}>
+                  <Video source={post.pUrl} id={post.pId} />
+                  <div className='fa' style={{ display: 'flex' }}>
                     <Avatar src={post.uProfile}></Avatar>
                     <h4>{post.uName}</h4>
                   </div>
-                  <Likes userData={userData} postData={post}/>
+                  <Likes userData={userData} postData={post} />
                   <ChatBubbleIcon onClick={() => handleClickOpen(post.pId)} className={`${classes.ci} icon-styling`} />
-                      <Dialog maxWidth="md" onClose={handleClose} aria-labelledby="customized-dialog-title" open={openId === post.pId}>
-                        <MuiDialogContent>
-                          <div className='dcontainer'>
-                            <div className='video-part'>
-                              <video  autoPlay={true} className='video-styles2' controls id={post.id} muted="muted" type="video/mp4" >
-                                <source src={post.pUrl} type="video/webm" />
-                              </video>
-                            </div>
-                            <div className='info-part'>
-                              <Card>
-                                <CardHeader
-                                  avatar={
-                                    <Avatar src={post?.uProfile} aria-label="recipe" className={classes.avatar}>
-                                    </Avatar>
-                                  }
-                                  action={
-                                    <IconButton aria-label="settings">
-                                      <MoreVertIcon />
-                                    </IconButton>
-                                  }
-                                  title={post?.uName}
+                  <Dialog maxWidth="md" onClose={handleClose} aria-labelledby="customized-dialog-title" open={openId === post.pId}>
+                    <MuiDialogContent>
+                      <div className='dcontainer'>
+                        <div className='video-part'>
+                          <video autoPlay={true} className='video-styles2' controls id={post.id} muted="muted" type="video/mp4" >
+                            <source src={post.pUrl} type="video/webm" />
+                          </video>
+                        </div>
+                        <div className='info-part'>
+                          <Card>
+                            <CardHeader
+                              avatar={
+                                <Avatar src={post?.uProfile} aria-label="recipe" className={classes.avatar}>
+                                </Avatar>
+                              }
+                              action={
+                                <IconButton aria-label="settings">
+                                  <MoreVertIcon />
+                                </IconButton>
+                              }
+                              title={post?.uName}
 
-                                />
-                                
-                                <hr style={{ border: "none", height: "1px", color: "#dfe6e9", backgroundColor: "#dfe6e9" }} />
-                                <CardContent className={classes.seeComments}>
-                                  
-                                <Comments userData={userData} postData={post} />
-                                </CardContent>
-                                
-                              </Card>
-                              <div className='extra'>
-                              <div className='likes'>
-                                <Typography className={classes.typo} variant='body2'>Liked By {post.likes.length == 0 ? 'nobody' : ` others`}</Typography>
-                                </div>
-                                <AddComment  userData={userData} postData={post}/> 
-                                </div>
+                            />
+
+                            <hr style={{ border: "none", height: "1px", color: "#dfe6e9", backgroundColor: "#dfe6e9" }} />
+                            <CardContent className={classes.seeComments}>
+
+                              <Comments userData={userData} postData={post} />
+                            </CardContent>
+
+                          </Card>
+                          <div className='extra'>
+                            <div className='likes'>
+                              <Typography className={classes.typo} variant='body2'>Liked By {post.likes.length == 0 ? 'nobody' : ` others`}</Typography>
                             </div>
+                            <AddComment userData={userData} postData={post} />
                           </div>
-                        </MuiDialogContent>
-                      </Dialog>
+                        </div>
+                      </div>
+                    </MuiDialogContent>
+                  </Dialog>
                 </div>
 
                 <div className='place'></div>
@@ -179,9 +179,9 @@ function Posts({userData=null}) {
           }
 
         </div>
-        }
-        </>
-    )
+      }
+    </>
+  )
 }
 
 export default Posts
---
+++ 03.React/reels/src/components/Video.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Video.css'
+import ReactDOM from 'react-dom';
+function Video(props) {
+    const handleMute = (e)=>{
+        e.preventDefault();
+        e.target.muted = !e.target.muted;
+    }
+    // vdo end hgayi to next vdo pr chalo
+    const handleAutoScroll= (e)=>{
+        let next = ReactDOM.findDOMNode(e.target).parentNode.nextSibling;
+        if(next)
+        {
+            next.scrollIntoView({behaviour:'smooth'});
+            e.target.muted = true;
+        }
+    }
+    return (
+        <>
+        <video onEnded={handleAutoScroll} src={props.source} className='video-styles' onClick={handleMute} muted='muted' type='video/mp4' ></video>
+        </>
+    )
+}
+
+export default Video

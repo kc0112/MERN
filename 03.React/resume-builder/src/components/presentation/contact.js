b55b3f180a754568a86c80137001c2cb4653511b
Khushi Chavada <khushichavada0112@gmail.com>
2021-08-02 17:56:34 +0530

    add

--- 03.React/resume-builder/src/components/presentation/contact.js
+++ 03.React/resume-builder/src/components/presentation/contact.js
@@ -1,7 +1,7 @@
-import React,{useState,useEffect} from "react";
-import {NavLink} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 // import update from 'immutability-helper';
-import {fieldCd, skinCodes}  from '../../constants/typeCodes';
+import { fieldCd, skinCodes } from '../../constants/typeCodes';
 import * as contactActions from '../../actions/contactActions';
 // import { bindActionCreators } from 'redux';
 // import { withRouter } from "react-router-dom";
@@ -10,27 +10,27 @@ import ResumePreview from './resumePreview'
 import { connect } from "react-redux";
 
 function Contact(props) {
-   let history = useHistory();
-   const [contact,setContact]= useState(props.contactSection);
-   useEffect(() => {
-       if(!props.document || !props.document.id || !props.document.skinCd)
-       {
-           history.push('/getting-started')
-       }
-   }, [])
-  
- 
-  const onchange=(event)=>{
-        var key =event.target.name;
-        var val =event.target.value;
+    let history = useHistory();
+    const [contact, setContact] = useState(props.contactSection);
+    useEffect(() => {
+        if (!props.document || !props.document.id || !props.document.skinCd) {
+            history.push('/getting-started')
+        }
+    }, [])
+
+
+    const onchange = (event) => {
+        var key = event.target.name;
+        var val = event.target.value;
         // this.setState({contactSection:update(this.state.contactSection,{$merge: {[key]:val}})});
-        setContact({...contact,[key]:val})
+        // variable form me object key lete to [] me enclose krte
+        setContact({ ...contact, [key]: val })
     }
-    const onSubmit= async()=>{
-        if(props.contactSection!=null){
+    const onSubmit = async () => {
+        if (props.contactSection != null) {
             props.updateContact(contact);
         }
-        else{
+        else {
             props.addContact(contact);
         }
 
@@ -38,86 +38,86 @@ function Contact(props) {
     }
 
 
-    const getFieldData=(key)=>{
-        if(contact && contact[key]){
-          return contact[key]
+    const getFieldData = (key) => {
+        if (contact && contact[key]) {
+            return contact[key]
         }
         return "";
     }
-    
+
     return (
-          <div className="container med contact">
+        <div className="container med contact">
             <div className="section funnel-section">
                 <div className="form-card">
                     <h2 className="form-heading center">Personal Details</h2>
                     <div className="form-section">
                         <div className="input-group"><label>First Name</label>
-                            <div className="effect"><input type="text" name={fieldCd.FirstName} value={getFieldData(fieldCd.FirstName)}  onChange={onchange}  /><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.FirstName} value={getFieldData(fieldCd.FirstName)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>Last Name</label>
-                            <div className="effect"><input type="text" name={fieldCd.LastName}  value={getFieldData(fieldCd.LastName)}  onChange={onchange}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.LastName} value={getFieldData(fieldCd.LastName)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group full"><label>Professional Summary</label>
-                            <div className="effect"><input type="text" name={fieldCd.ProfSummary}   value={getFieldData(fieldCd.ProfSummary)}  onChange={onchange}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.ProfSummary} value={getFieldData(fieldCd.ProfSummary)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>Email</label>
-                            <div className="effect"><input type="text"  name={fieldCd.Email}  value={getFieldData(fieldCd.Email)}  onChange={onchange} /><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.Email} value={getFieldData(fieldCd.Email)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>Phone</label>
-                            <div className="effect"><input type="text"  name={fieldCd.Phone}   value={getFieldData(fieldCd.Phone)}  onChange={onchange}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.Phone} value={getFieldData(fieldCd.Phone)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>Profession</label>
-                            <div className="effect"><input type="text"  name={fieldCd.Profession}  value={getFieldData(fieldCd.Profession)}  onChange={onchange}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.Profession} value={getFieldData(fieldCd.Profession)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
                         <div className="input-group"><label>Street</label>
-                            <div className="effect"><input type="text" name={fieldCd.Street}   value={getFieldData(fieldCd.Street)}  onChange={onchange}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.Street} value={getFieldData(fieldCd.Street)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>City</label>
-                            <div className="effect"><input type="text" name={fieldCd.City}  value={getFieldData(fieldCd.City)}  onChange={onchange}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.City} value={getFieldData(fieldCd.City)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
                         <div className="input-group"><label>State</label>
-                            <div className="effect"><input type="text"   name={fieldCd.State}  value={getFieldData(fieldCd.State)}  onChange={onchange} /><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.State} value={getFieldData(fieldCd.State)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
 
 
                         <div className="input-group"><label>Country</label>
-                            <div className="effect"><input type="text"  name={fieldCd.Country}  value={getFieldData(fieldCd.Country)}  onChange={onchange}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.Country} value={getFieldData(fieldCd.Country)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
                         <div className="input-group"><label>Pin Code</label>
-                            <div className="effect"><input type="text" name={fieldCd.ZipCode}  value={getFieldData(fieldCd.ZipCode)}  onChange={onchange}/><span></span>
+                            <div className="effect"><input type="text" name={fieldCd.ZipCode} value={getFieldData(fieldCd.ZipCode)} onChange={onchange} /><span></span>
                             </div>
                             <div className="error"></div>
                         </div>
                         <div className="form-buttons">
                             <button onClick={onSubmit} className="btn hvr-float-shadow" type='button'>Next</button>
-                            <NavLink to='/getting-started'  className="center">Back</NavLink>
+                            <NavLink to='/getting-started' className="center">Back</NavLink>
                         </div>
                     </div>
 
@@ -131,18 +131,18 @@ function Contact(props) {
         </div>
     );
 }
-const mapStateToProps= (state)=>{
+const mapStateToProps = (state) => {
     return {
-        contactSection:state.contactSection,
-        document:state.document
+        contactSection: state.contactSection,
+        document: state.document
     }
 }
-const mapDispatchToProps = dispatch=>{
+const mapDispatchToProps = dispatch => {
     return {
-        addContact:(contact)=>dispatch(contactActions.add(contact)),
-        updateContact:(contact)=>dispatch(contactActions.update(contact))
-    } 
+        addContact: (contact) => dispatch(contactActions.add(contact)),
+        updateContact: (contact) => dispatch(contactActions.update(contact))
+    }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Contact)
+export default connect(mapStateToProps, mapDispatchToProps)(Contact)
 
